38046c73aa311055b7dabceaca61e8b9bf1173c8
ivncstnd <ivanalexandercastaneda@gmail.com>
2021-02-12 03:47:35 -0800

    comments

--- CS010B/week6/IntVector/IntVector.cpp
+++ CS010B/week6/IntVector/IntVector.cpp
@@ -6,34 +6,38 @@
 using namespace std;
 
 /* Constructor */
+// Variables are adjusted via overloaded constructor initalized in header file
+// Assign variables to respective private member 
+// Initalize array values given appropriate capacity (e.g. not null)
 IntVector::IntVector(unsigned capacity, int value) {
     _capacity = capacity;
     _size = capacity;
+    _data = new int [capacity];
     if(capacity != 0) {
-        _data = new int [capacity];
         for (unsigned i = 0; i < capacity; ++i) {
             _data[i] = value;
         }
     }
-    else {
-        _data = new int [0];
-    }
 }
 
 /* Deconstructor */
+// Frees dynamic array from the heap
 IntVector::~IntVector() {
     delete [] _data;
 }
 
 /* Accessors */
+// Access private member size
 unsigned IntVector::size() const {
     return _size;
 }
 
+// Access private member capacity
 unsigned IntVector::capacity() const {
     return _capacity;
 }
 
+// Checks to see if the vector is empty
 bool IntVector::empty() const {
     if (_size == 0) {
         return true;
@@ -43,6 +47,9 @@ bool IntVector::empty() const {
     }
 }
 
+// Returns an immutable value from array from index
+// Checks to see if index is out of bounds
+// If true, throw out of range exception
 const int & IntVector::at(unsigned index) const {
     if (index >= _size) {
         throw out_of_range("IntVector::at_range_check");
@@ -50,15 +57,20 @@ const int & IntVector::at(unsigned index) const {
     return _data[index];
 }
 
+// Returns an immutable first value from array
 const int & IntVector::front() const {
     return _data[0];
 }
 
+// Returns an immutable last value from array
 const int & IntVector::back() const {
     return _data[_size-1];
 }
 
 /* Mutators */
+// Inserts a value into the array on any appropriate index 
+// Adjusts size and capacity as needed
+// To insert, value is inserted at the end of array and swaps to its correct index
 void IntVector::insert(unsigned index, int value) {
     if (index > _size) {
         throw out_of_range("IntVector::insert_range_check");
@@ -78,6 +90,8 @@ void IntVector::insert(unsigned index, int value) {
     }
 }
 
+// Removes a value from the array from any appropriate index
+// To erase, value is swapped to the ned of the array and the size decremented, losing access to the value
 void IntVector::erase(unsigned index) {
     if (index >= _size) {
         throw out_of_range("IntVector::erase_range_check");
@@ -90,6 +104,8 @@ void IntVector::erase(unsigned index) {
     --_size;
 }
 
+// Appends to a value to the end of an array
+// Modifies capacity as needed
 void IntVector::push_back(int value) {
     if (_capacity == 0) {
         expand(1);
@@ -101,14 +117,20 @@ void IntVector::push_back(int value) {
     ++_size;
 }
 
+// Removes a value from the end of an array 
+// Does not check for out of range
 void IntVector::pop_back() {
     --_size;
 }
 
+// Removes all values from a vector
 void IntVector::clear() {
     _size = 0;
 }
 
+// Resizes the vector to the according size value
+// Checks whether the capacity is large via double or appending the new size
+// Passes a optional value assignment once a resize has occured
 void IntVector::resize(unsigned size, int value) {
         if (_size == 0) {
         expand(size);
@@ -133,12 +155,14 @@ void IntVector::resize(unsigned size, int value) {
     _size = size;
 }
 
+// Reserves a specified capacity
 void IntVector::reserve(unsigned n) {
     if(n >= _capacity) {
         expand(n - _capacity);
     } 
 }
 
+// Reserves a specified capcity and fills capacity with value parameter
 void IntVector::assign(unsigned n, int value) {
     _size = n;
     if (n > _capacity) {
@@ -154,6 +178,7 @@ void IntVector::assign(unsigned n, int value) {
     }
 }
 
+// Returns a mutable value from array from index
 int & IntVector::at(unsigned index) {
     if (index >= _size) {
         throw out_of_range("IntVector::at_range_check");
@@ -161,15 +186,22 @@ int & IntVector::at(unsigned index) {
     return _data[index];
 }
 
+// Returns a mutable first value from array
 int & IntVector::front() {
     return _data[0];
 }
 
+// Returns a mutable last value from array
 int & IntVector::back() {
     return _data[_size-1];
 }
 
 /* Private Helpers */
+// Expand raises the total capacity of the dynamic array when called
+// Because vectors are flexible with size calls, expand allocates needed memory to hold such values
+// The functions create a new dynamic array with a adjusted capacity cap
+// Values stored in the old dynamic array are copied into the new one then letting the old array be freed
+// The capacity change is offically declared and the temporary refit array is pointed to the private member dynamic array
 void IntVector::expand() {
     int* refit = new int [_capacity * 2];
     for (unsigned i = 0; i < _size; ++i) {
