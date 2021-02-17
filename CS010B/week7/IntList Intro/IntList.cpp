8789e31e72343bbbaad5633c5e5b643786663ec1
ivan <ivanalexandercastaneda@gmail.com>
2021-02-16 20:00:12 -0800

    functions explained

--- CS010B/week7/IntList Intro/IntList.cpp
+++ CS010B/week7/IntList Intro/IntList.cpp
@@ -3,11 +3,16 @@
 
 using namespace std;
 
+/* Constructor */
+// Create an empty list by assigning the head and tail as null nodes
 IntList::IntList() {
     head = NULL;
     tail = NULL;
 }
 
+/* Deconstructor */
+// Deleting the list requires going through each element
+// and deleting the element, freeing one by one
 IntList::~IntList() {
     IntNode* temp;
     while(head) {
@@ -17,18 +22,26 @@ IntList::~IntList() {
 	}
 }
 
+/* Mutators */
+// Pushes a new element to the front of the list
+// By creating a new node, the new node will become the head
 void IntList::push_front(int value) {
     IntNode* front = new IntNode(value);
     front -> next = head;
     head = front;
 }
 
+// Removes the first element from the list
+// By assigning the original head to a new node and making the next node
+// the head allowing the deletion of the original head
 void IntList::pop_front() {
     IntNode* front = head;
     head = head -> next;
     delete front;
 }
 
+/* Accessors */
+// Checks if the head node is null
 bool IntList::empty() const {
     if(head == NULL) {
         return true;
@@ -36,21 +49,15 @@ bool IntList::empty() const {
     return false;
 }
 
+// Returns the head node's value
 const int & IntList::front() const {
     return head -> value;
 }
 
+// Iterates through the list until the tail is found
+// if the tail is found, last node is found
+// return the value of the last node in the list
 const int & IntList::back() const {
-    /*
-    IntNode* temp = head;
-    IntNode* last;
-    while (temp != NULL) {
-        last = temp;
-        temp = temp -> next;
-    }
-    return last -> value;
-    */
-
     IntNode* last;
     for(IntNode* temp = head; temp != NULL; temp = temp -> next) {
         last = temp;
@@ -58,6 +65,9 @@ const int & IntList::back() const {
     return last -> value;
 }
 
+// Iterates through the list and outputs the value at the node
+// if the tail is found, output the last node's value no whitespace
+// and return the list
 ostream & operator<<(ostream & out, const IntList & list) {
     IntNode* temp = list.head;
     while(temp != NULL) {
