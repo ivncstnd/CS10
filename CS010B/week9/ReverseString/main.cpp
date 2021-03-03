465f67ac93d77ebe0bf9e17a7f4ad2b3676109e6
ivncstnd <ivanalexandercastaneda@gmail.com>
2021-03-02 21:59:34 -0800

    recursive string reverse comments

--- CS010B/week9/ReverseString/main.cpp
+++ CS010B/week9/ReverseString/main.cpp
@@ -22,6 +22,12 @@ void flipString(string &s) {
    if(s.size() <= 1) {
       return;
    }
+   /**
+    * This func recurses through flipString funcs until it hits base case.
+    * Once the base case is hit, each case before swaps the first and last
+    * characters of the inputted string parameter. Each returns a string with
+    * the first and last characters swapped until it reaches the first func call.
+    */
    string temp = s.substr(1, s.size() - 2);
    flipString(temp);
    s = s[s.size() - 1] + temp + s[0];
