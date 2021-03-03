cb78933b99c7574c52977bfed9cc44876271f757
ivncstnd <ivanalexandercastaneda@gmail.com>
2021-03-02 21:55:57 -0800

    recursion

--- CS010B/week9/ReverseString/main.cpp
+++ CS010B/week9/ReverseString/main.cpp
@@ -14,41 +14,18 @@ int main() {
    cout << line << endl;
    flipString(line);
    cout << line << endl;
-
    return 0;
 }
 
-/*
 //Implement the flipString function here
 void flipString(string &s) {
-   string temp = " ";
    if(s.size() <= 1) {
       return;
    }
-   else {
-      temp = s.substr(1, s.size() - 2);
-      flipString(temp);
-      s = s[s.size() - 1] + temp + s[0];
-   }
+   string temp = s.substr(1, s.size() - 2);
+   flipString(temp);
+   s = s[s.size() - 1] + temp + s[0];
    return;
 }
-*/
 
-void flipString(string &s) {
-   if(s.length() <= 1) {
-      return;
-   }
-   else {
-      return flipString(s.subtr(1, s.length())) + s.at(0);
-   }
-}
-
-/**
- * hello
- * olleh
- * swap beginning and end until can not swap
- * 
- * 
- * 
- */
 
