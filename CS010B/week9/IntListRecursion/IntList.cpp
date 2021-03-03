5104e7546e7dc74baa81002987d9769958b7c96d
ivncstnd <ivanalexandercastaneda@gmail.com>
2021-03-02 23:48:50 -0800

    intlist recursion

--- CS010B/week9/IntListRecursion/IntList.cpp
+++ CS010B/week9/IntListRecursion/IntList.cpp
@@ -16,3 +16,49 @@ void IntList::push_front(int val) {
       head = temp;
    }
 }
+
+/* Outputs to a single line all of the int values stored in the list, each separated by a space. 
+   This function does NOT output a newline or space at the end.
+*/
+ostream & operator<<(ostream & out, const IntList & list) {
+   if(list.head == 0) {
+      return out;
+   }
+   operator<<(out, list.head);
+   return out;
+}
+
+/* Returns true if the integer passed in is contained within the IntList.
+   Otherwise returns false.
+*/
+bool IntList::exists(int value) const {
+   if(head == 0) {
+      return false;
+   }
+   if(head -> value == value) {
+      return true;
+   }
+   return exists(head -> next, value);
+}
+
+bool IntList::exists(IntNode *node, int value) const {
+   if (node == 0) {
+      return false;
+   }
+   if(node -> value == value) {
+      return true;
+   }
+   return exists(node -> next, value);
+}
+
+ostream & operator<<(ostream & out, IntNode * list) {
+   if(list == 0) {
+      return out;
+   }
+   out << list -> value;
+   if (list -> next != 0) {
+      out << " ";
+   }
+   operator<<(out, list -> next);
+   return out;
+}
